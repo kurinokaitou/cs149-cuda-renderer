2e352504ec33d8d1dd741b26c5732f1cb8c86986
kurino <gejingze@163.com>
2023-05-26 10:20:33 +0800

    feat: finish part1

--- saxpy/main.cpp
+++ saxpy/main.cpp
@@ -6,7 +6,6 @@
 void saxpyCuda(int N, float alpha, float* x, float* y, float* result);
 void printCudaInfo();
 
-
 void usage(const char* progname) {
     printf("Usage: %s [options]\n", progname);
     printf("Program Options:\n");
@@ -14,23 +13,18 @@ void usage(const char* progname) {
     printf("  -?  --help             This message\n");
 }
 
-
-int main(int argc, char** argv)
-{
-
+int main(int argc, char** argv) {
     // default: arrays of 100M numbers
     int N = 100 * 1000 * 1000;
 
     // parse commandline options ////////////////////////////////////////////
     int opt;
     static struct option long_options[] = {
-        {"arraysize",  1, 0, 'n'},
-        {"help",       0, 0, '?'},
-        {0 ,0, 0, 0}
-    };
+        {"arraysize", 1, 0, 'n'},
+        {"help", 0, 0, '?'},
+        {0, 0, 0, 0}};
 
     while ((opt = getopt_long(argc, argv, "?n:", long_options, NULL)) != EOF) {
-
         switch (opt) {
         case 'n':
             N = atoi(optarg);
@@ -48,21 +42,21 @@ int main(int argc, char** argv)
     float* yarray = new float[N];
     float* resultarray = new float[N];
 
-    for (int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
         xarray[i] = yarray[i] = i % 10;
         resultarray[i] = 0.f;
-   }
-
-    printCudaInfo();
-    
-    printf("Running 3 timing tests:\n");
-    for (int i=0; i<3; i++) {
-      saxpyCuda(N, alpha, xarray, yarray, resultarray);
     }
 
-    delete [] xarray;
-    delete [] yarray;
-    delete [] resultarray;
+    printCudaInfo();
+
+    printf("Running 3 timing tests:\n");
+    for (int i = 0; i < 3; i++) {
+        saxpyCuda(N, alpha, xarray, yarray, resultarray);
+    }
+
+    delete[] xarray;
+    delete[] yarray;
+    delete[] resultarray;
 
     return 0;
 }
